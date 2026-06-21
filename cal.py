2977b18966e6de561edabe6258308fe24baffca8
Upeksha Wickramasinghe <upekshawikcramasinghe2005@gmail.com>
2026-06-21 09:54:22 +0530

    Update cal.py

--- cal.py
+++ cal.py
@@ -1,5 +1,18 @@
 num1= float(input('Enter no : ' ))
 num2=float(input('Enter no : ' ))
 simbol=input('+,-,*,/')
-answer
+if simbol==+:
+  num1+num2
+elif simbol==-:
+   num1-num2
+elif simbol==*:
+   num1*num2
+else:
+  
+   num1/num2
+
+  
+  
+  
+
 
